dd0727d779cdd16350d927423290d651e30be519
Jay-Ferguson <jayfergusonweb@gmail.com>
2025-06-19 23:35:58 -0400

    updated types/react shared dep v19.1.3 added env url for production and preview builds to fix vercel build error

--- apps/web/src/config.ts
+++ apps/web/src/config.ts
@@ -1,8 +1,11 @@
 export const getBaseUrl = () => {
-  if (process.env.VERCEL_ENV === "production") {
+  if (
+    process.env.VERCEL_ENV === "production" &&
+    process.env.VERCEL_PROJECT_PRODUCTION_URL
+  ) {
     return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
   }
-  if (process.env.VERCEL_ENV === "preview") {
+  if (process.env.VERCEL_ENV === "preview" && process.env.VERCEL_URL) {
     return `https://${process.env.VERCEL_URL}`;
   }
 
