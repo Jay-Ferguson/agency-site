1c82efae88925e57967e50c0427a71db7f88540c
Jay-Ferguson <jayfergusonweb@gmail.com>
2026-02-26 20:28:12 -0500

    added globals for node.process in posthog.js

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,4 +1,14 @@
 import { nextJsConfig } from "@workspace/eslint-config/next-js";
-
+import globals from "globals";
 /** @type {import("eslint").Linter.Config} */
-export default nextJsConfig;
+export default [
+  nextJsConfig,
+  {
+    files: ["app/**/*.js', 'app/**/*.ts"], // Only target the app folder
+    languageOptions: {
+      globals: {
+        ...globals.node, // Adds all Node.js globals
+      },
+    },
+  },
+];
---
+++ apps/web/src/app/posthog.js
@@ -0,0 +1,16 @@
+// eslint-env node
+import "dotenv/config";
+import "server-only";
+
+// app/posthog.js
+import { PostHog } from "posthog-node";
+
+export default function PostHogClient() {
+  // eslint-disable-next-line no-undef
+  const posthogClient = new PostHog(process.env.NEXT_PUBLIC_POSTHOG_TOKEN, {
+    host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    flushAt: 1,
+    flushInterval: 0,
+  });
+  return posthogClient;
+}
