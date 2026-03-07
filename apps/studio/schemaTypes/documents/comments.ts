26172905433289a0b6e8ab699dccce2141aad8ee
Jay-Ferguson <jayfergusonweb@gmail.com>
2026-03-07 11:37:31 -0500

    formatting in comments.ts file.

--- apps/studio/schemaTypes/documents/comments.ts
+++ apps/studio/schemaTypes/documents/comments.ts
@@ -1,23 +1,25 @@
-import { richTextField } from "../common";
 import { text } from "node:stream/consumers";
+
+import { richTextField } from "../common";
 
 export default {
   name: "comments",
   type: "document",
   title: "Comments",
   description: "Manage comments on your content",
-  fields: [{
+  fields: [
+    {
       name: "name",
-      type: "string", 
-  }, {
-     name:'commentBody',
-      type:"text"
-
-  },
-{
-name:'canBePublished',
-type:'reference',
-to: [{ type: "blog" }, { type: "page" }],
-}
-]
-}
+      type: "string",
+    },
+    {
+      name: "commentBody",
+      type: "text",
+    },
+    {
+      name: "canBePublished",
+      type: "reference",
+      to: [{ type: "blog" }, { type: "page" }],
+    },
+  ],
+};
